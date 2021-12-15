9240e75236274f80342ec93233bc6a0c60634b53
Jerry Tong <zihangtong@gmail.com>
2021-12-15 18:22:43 -0500

    commit tree 8f3a688fbeb177e33d1092d61d6ddbac72a4cc89

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 function Item({ name, category }) {
+
+  const [inCart, setCart] = useState(false);
+function handleClick() {
+  setCart(inCart => !inCart);
+}
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className={ inCart ? "in-cart" : ""}>{ inCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
